ed7d7365d1efceacb638cabc48ca708df3a4a150
Thijs Nissen <thijsnissen@gmail.com>
2024-01-25 19:42:13 +0100

    updated workflow with formatting check

--- project/Aliasses.scala
+++ project/Aliasses.scala
@@ -4,6 +4,6 @@ object Aliasses {
   lazy val common =
     addCommandAlias("format", "scalafmtAll;scalafmtSbt") ++
       addCommandAlias("check", "scalafmtCheckAll;scalafmtSbtCheck") ++
-    addCommandAlias("v", "format;compile;Test/compile") ++
+      addCommandAlias("v", "format;compile;Test/compile") ++
       addCommandAlias("vt", "validate;test")
 }
