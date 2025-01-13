b98d6674294fd3d8cb2894f8dc8a907ea1865b9c
Scala Steward <scala-steward@users.noreply.github.com>
2025-01-13 06:15:11 +0000

    Reformat with scalafmt 3.8.4
    Executed command: scalafmt --non-interactive

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,11 +1,13 @@
 import sbt.*
 
 object Dependencies {
-  lazy val common = Seq(
-  )
+  lazy val common =
+    Seq(
+    )
 
-  lazy val app = Seq(
-  )
+  lazy val app =
+    Seq(
+    )
 
   lazy val test = Seq(
     "org.scalatest" %% "scalatest" % Version.scalatest % "test"
