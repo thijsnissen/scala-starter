78d86205878277a416ed3ed215537b8204f445d6
Thijs Nissen <thijsnissen@gmail.com>
2026-01-23 09:27:12 +0100

    add @main method

--- code/app/src/main/scala/Main.scala
+++ code/app/src/main/scala/Main.scala
@@ -2,9 +2,10 @@ import scala.io.BufferedSource
 import scala.io.Source
 import scala.util.Using
 
-object Main extends App:
+object Main:
   lazy val msg: String =
     Using.resource(Source.fromResource("myResource.txt")):
       (b: BufferedSource) => b.mkString.trim
 
-  println(msg)
+  @main def run(): Unit =
+    println(msg)
